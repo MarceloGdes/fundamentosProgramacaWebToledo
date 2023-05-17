b2d74e5faf7c159e8a3d0ade725074040c21ec06
Marcelo <guedesmarcello733@outlook.com>
2023-05-17 19:35:37 -0300

    Adicionando a aula e exercicios de Variaveis E HTML DOM

--- Aulas/2st bimestre/Variaveis/Variaveis e DOM/Exercicios/EX01/scripts/script.js
+++ Aulas/2st bimestre/Variaveis/Variaveis e DOM/Exercicios/EX01/scripts/script.js
@@ -4,12 +4,18 @@ let nameInput = document.querySelector("#name");
 let surnameInput = document.querySelector("#surname");
 let buttom = document.querySelector("#btm");
 
+
 function transformTitle() {
     let newName = String(nameInput.value)
     let newSurname = String(surnameInput.value)
-    completeNameTitle.textContent = newName + ' ' + newSurname;
+    //completeNameTitle.textContent += newName + '' + newSurname;
+    // prorpiedade innerHTML atribui elementos no HTML
+    completeNameTitle.innerHTML += newName + '' + newSurname + "<br>";
+
 }
 
 buttom.onclick = () => {
    transformTitle();
+   console.log(completeNameTitle.__proto__)
 }
+
